d3efcac3d2e008cd2a9a75d0db53bb6d61ccf61f
Disha Anand <102431437+DishaAnand@users.noreply.github.com>
2022-06-16 14:37:09 +0530

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const data = [
+    {
+      value: 1,
+      label: "Disha"
+    },
+    {
+      value: 2,
+      label: "Anand"
+    },
+    {
+      value: 3,
+      label: "Credo"
+    },
+    {
+      value: 4,
+      label: "Health"
+    },
+    {
+      value: 5,
+      label: "Awesome Interviewer"
+    },
+    {
+      value: 6,
+      label: "Kalpana Ji"
+    },
+    {
+      value: 7,
+      label: "Disha Bajaj"
+    }
+  ];
